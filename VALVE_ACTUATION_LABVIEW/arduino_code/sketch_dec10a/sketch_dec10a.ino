d3d56d99aca669d77b39f4bded9fb3a81a548dbc
Viraj Jayasinghe <66692075+VirajJay@users.noreply.github.com>
2020-06-10 15:16:10 +1000

    Add files via upload

---
+++ VALVE_ACTUATION_LABVIEW/arduino_code/sketch_dec10a/sketch_dec10a.ino
@@ -0,0 +1,21 @@
+int trg_1=11;
+int pwr_1=53;
+void setup() {
+  // put your setup code here, to run once:
+
+//  pinMode(trg_1, OUTPUT);
+  //pinMode(pwr_1, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /*
+  digitalWrite(pwr_1, LOW);
+  digitalWrite(trg_1, LOW);
+  digitalWrite(trg_1, HIGH);
+  delay(2000);
+  digitalWrite(pwr_1, HIGH); 
+  delay(1000);
+  */
+}
